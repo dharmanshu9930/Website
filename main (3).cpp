43b06ed50e57c9ff47e826661dd73e9a79db760d
MK469 <113232126+MK469@users.noreply.github.com>
2022-10-07 21:57:24 +0530

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    for(i=1;i<=25;i++)
+    {
+        for(n=1;n<=i;n++)
+        {cout<<"@";}
+        
+        {cout<<endl;}
+    }
+    return 0; 
+    
+        
+    }
+   
+    
