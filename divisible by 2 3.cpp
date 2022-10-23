62846a05aa7c54e086fab5c3173ae5c1311b798d
joshiaarushi <115158160+joshiaarushi@users.noreply.github.com>
2022-10-23 23:02:34 +0530

    hactoberfest2022

---
+++ divisible by 2 3.cpp
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main()
+{
+int a=0;
+printf("enter a no.");
+scanf("%d",&a);
+if ( a%3==0 and a%2==0)
+{
+printf(" no is divisible");
+}
+else
+{
+printf("no is not divisible");
+}
+}
