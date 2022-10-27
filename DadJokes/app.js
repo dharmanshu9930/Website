f891b0186a2d399425aa4e095b2d97d86f1d96e0
Asrani-Aman <96644946+Asrani-Aman@users.noreply.github.com>
2022-10-27 18:39:34 +0530

    Telling Jokes Through AJAX API

---
+++ DadJokes/app.js
@@ -0,0 +1,21 @@
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+};
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
+  } catch (e) {
+    return "NO JOKES AVAILABLE! SORRY :(";
+  }
+};
+
+button.addEventListener("click", addNewJoke);
